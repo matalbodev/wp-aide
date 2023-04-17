47494bc30b8ce6e29d226bd2554b7a45e29dd2fd
matalbodev <mathieualbore@gmail.com>
2023-04-17 13:20:50 +0200

    Update (parse) parse and replace with dynamic components

---
+++ wp-aide-frontend/src/components/Index.tsx
@@ -0,0 +1,50 @@
+import SearchBar from "./SearchBar";
+import Offers from "./Offers";
+import { HTMLReactParserOptions, Element, domToReact } from "html-react-parser";
+import parse from "html-react-parser";
+import ContactForm from "./ContactForm";
+import Button from "./Button";
+
+interface Props {
+	contentString: string;
+	offers: [];
+}
+
+const Index = (props: Props) => {
+	const { contentString: content, offers } = props;
+	const options: HTMLReactParserOptions = {
+		replace: (domNode) => {
+			if (domNode instanceof Element && domNode.attribs) {
+				const { name, children, attribs } = domNode;
+				if (attribs.id === "domainsSearchBar") {
+					return <SearchBar />;
+				}
+				if (attribs.id === "offersList") {
+					return <Offers offers={offers} />;
+				}
+				if (attribs.id === "contactForm") {
+					return <ContactForm />;
+				}
+				if (name === "a" && attribs.class.includes("wp-block-button")) {
+					const { href } = attribs;
+					const srcClass = attribs.class;
+					let color: "yellow" | "blue";
+					if (srcClass.includes("has-strong-blue-background-color")) {
+						color = "blue";
+					} else {
+						color = "yellow";
+					}
+					return (
+						<Button color={color} to={href}>
+							{domToReact(children)}
+						</Button>
+					);
+				}
+			}
+		},
+	};
+
+	return <>{content && parse(content, options)}</>;
+};
+
+export default Index;
