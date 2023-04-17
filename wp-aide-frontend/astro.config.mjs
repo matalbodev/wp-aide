66b2745a8e040657235e67367b810bbad05378b8
matalbodev <mathieualbore@gmail.com>
2023-04-17 13:20:22 +0200

    Upate config and packages

--- wp-aide-frontend/astro.config.mjs
+++ wp-aide-frontend/astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+	output: "server",
+	integrations: [tailwind(), react()],
+	vite: {
+		plugins: [basicSsl()],
+	},
 });
