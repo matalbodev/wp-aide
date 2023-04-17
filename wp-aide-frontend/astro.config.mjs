8386ba155a7a4f74a13f8b282cfab84d9bcdbd52
matalbodev <mathieualbore@gmail.com>
2023-04-17 13:36:07 +0200

    Update (vercel) add vercel

--- wp-aide-frontend/astro.config.mjs
+++ wp-aide-frontend/astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [tailwind(), react()],
-	vite: {
-		plugins: [basicSsl()],
-	},
+  output: "server",
+  integrations: [tailwind(), react()],
+  vite: {
+    plugins: [basicSsl()]
+  },
+  adapter: vercel()
 });
