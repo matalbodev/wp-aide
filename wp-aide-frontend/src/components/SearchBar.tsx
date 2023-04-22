bc1ffa53d0924aa9f13387273deac8e738f45be6
matalbodev <mathieualbore@gmail.com>
2023-04-22 12:49:18 +0200

    Update pretify

--- wp-aide-frontend/src/components/SearchBar.tsx
+++ wp-aide-frontend/src/components/SearchBar.tsx
@@ -1,54 +1,83 @@
+import clsx from "clsx";
 import { useState, type FormEvent } from "react";
+import Button from "./Button";
+import useScreenSize from "../hooks/useScreenSize";
 
 enum Availability {
-	available = "available",
-	unavailable = "unavailable",
+  available = "available",
+  unavailable = "unavailable",
 }
 
-const responses = {
-	default: "Recherche",
-	available: "Je me lance !",
-	unavailable: "Domaine indispo",
-};
-
 export default function SearchBar() {
-	const [isSearching, setIsSearching] = useState<boolean>(false);
-	const [isAvailable, setIsAvailable] = useState<Availability | undefined>();
-	const [isFilled, setIsFilled] = useState(false);
-	const handleSentform = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
-		e.preventDefault();
-		setIsSearching(true);
-		const target = e.currentTarget;
-		const data = new FormData(target);
-		const url = data.get("domain") as string;
-		const response = await fetch(`/api/domain?name=${url}`, {
-			method: "GET",
-		});
-		const json: {
-			products: {
-				name: string;
-				status: Availability;
-			}[];
-		} = await response.json();
-		setIsAvailable(Availability[json.products[0].status]);
-		setIsSearching(false);
-	};
+  const [isSearching, setIsSearching] = useState<boolean>(false);
+  const [isAvailable, setIsAvailable] = useState<Availability | undefined>();
+  const [isFilled, setIsFilled] = useState(false);
+  const { isMobile } = useScreenSize();
+  const handleSentform = async (
+    e: FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+    setIsSearching(true);
+    const target = e.currentTarget;
+    const data = new FormData(target);
+    const url = data.get("domain") as string;
+    const response = await fetch(`/api/domain?name=${url}`, {
+      method: "GET",
+    });
+    const json: {
+      products: {
+        name: string;
+        status: Availability;
+      }[];
+    } = await response.json();
+    setIsAvailable(Availability[json.products[0].status]);
+    setIsSearching(false);
+  };
+  const responses = {
+    available: (
+      <div className="mt-8 flex items-center bg-yellow-theme pl-4">
+        <div className="font-bold text-blue-theme">Domaine disponible</div>
+        <div className="ml-auto">
+          <Button
+            as="a"
+            to="#contact?subject=Reservation de domaine"
+            color="blue"
+          >
+            Demander la réservation
+          </Button>
+        </div>
+      </div>
+    ),
+    unavailable: (
+      <div className="mt-8 bg-yellow-theme px-4 py-3">
+        <div className="font-bold text-blue-theme">Domaine indisponible</div>
+      </div>
+    ),
+  };
 
-	const status = responses[isAvailable || "default"];
+  const status = responses[isAvailable as Availability];
 
-	return (
-		<form onSubmit={handleSentform} className="lg:-mr-24 flex items-center mt-8 shadow-xl">
-			<input
-				onChange={(e) => setIsFilled(e.target.value !== "")}
-				type="text"
-				placeholder="Je recherche un nom de domaine disponible pour mon site"
-				name="domain"
-				className="px-5 py-3 w-full flex-1 text-blue-theme"
-				id="domain"
-			/>
-			<button className="bg-yellow-theme text-blue-theme px-8 py-3 font-semibold" type="submit">
-				{isFilled ? (isSearching ? "Recherche en cours" : status) : "Recherche"}
-			</button>
-		</form>
-	);
+  return (
+    <>
+      <form
+        onSubmit={handleSentform}
+        className="mt-8 flex items-start lg:-mr-24"
+      >
+        <div className="flex-1">
+          <input
+            onChange={(e) => setIsFilled(e.target.value.length > 0)}
+            type="text"
+            placeholder="Je recherche un nom de domaine disponible pour mon site"
+            name="domain"
+            className="w-full flex-1 appearance-none rounded-none px-5 py-3 text-blue-theme shadow-xl"
+            id="domain"
+          />
+        </div>
+        <Button color="yellow" as="button">
+          {isSearching ? "Recherche en cours" : "Recherche"}
+        </Button>
+      </form>
+      {isFilled && status}
+    </>
+  );
 }
